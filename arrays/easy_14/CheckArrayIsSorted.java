af5d352005d847fb8f60237733303db19feb89b0
roshan5977 <roshannayak5977@gmail.com>
2024-06-26 22:10:53 +0530

    check arr is sorted

--- arrays/easy_14/CheckArrayIsSorted.java
+++ arrays/easy_14/CheckArrayIsSorted.java
@@ -2,4 +2,27 @@ package arrays.easy_14;
 
 public class CheckArrayIsSorted {
     
+    public static void main(String[] args) {
+        int[]  arr = {2,4,5,6,8,9};
+
+        checkArrSorted(arr);
+    }
+
+    private static void checkArrSorted(int[] arr) {
+        // TODO Auto-generated method stub
+        if(arr.length<2){
+            System.out.println("True");
+            return;
+           };
+        for(int i=1;i<arr.length;i++){
+           if(arr[i-1]>arr[i]){
+            System.out.println("False");
+            return;
+           }
+        }
+        System.out.println("True");
+
+    }
+
+   
 }
