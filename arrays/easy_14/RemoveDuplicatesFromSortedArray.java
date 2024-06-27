f1de95f2213a8ad32fa8bbb52365a5698a376036
roshan5977 <roshannayak5977@gmail.com>
2024-06-27 23:25:48 +0530

    sh

--- arrays/easy_14/RemoveDuplicatesFromSortedArray.java
+++ arrays/easy_14/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,16 @@
 package arrays.easy_14;
 
 public class RemoveDuplicatesFromSortedArray {
-    
+    public static void main(String[] args) {
+        int[] arr ={3,5,5,6,8,5,1,2,3};
+        int[] result= removeDuplicatesInArr(arr);
+    }
+
+    private static  int[] removeDuplicatesInArr(int[] arr) {
+        
+
+        return arr;
+    }
+
 }
+//n
